ede3b53dda00773ef65b78ba00c19f41f449ed14
Sudhanwa Pande <subhampande100@gmail.com>
2026-06-04 14:50:16 +0530

    bug fix

--- src/app/doctor/call/[appointmentId]/page.tsx
+++ src/app/doctor/call/[appointmentId]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
